cabec6ffedb8d48e88bbcd4dee68b70cb96b8760
Dev1 <bandgarmohan07@gmail.com>
2021-01-27 14:11:05 +0530

    HIS-301:Related changes

--- src/main/java/com/mit/app/Application.java
+++ src/main/java/com/mit/app/Application.java
@@ -6,12 +6,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+
+	private Map<String,Object> cache = new HashMap<String,Object>();
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 
 		int a=10;
 	}
 	
+
+	public void loadDataToCache(){
+
+		//logic
+	}
 	//HIS 2001 related changes
 	public void doProcess() {
 	
